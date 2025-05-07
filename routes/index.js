2f11b1f98e7d2bc5a139857f9b56c9ff1569ac34
Bjorn Nilsson <Bjorn.nilsson1984@gmail.com>
2025-05-07 18:33:42 +0200

    add function to add products to DB

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 var express = require("express");
 var router = express.Router();
 var Database = require("better-sqlite3");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const db = new Database("./data/data.db", {
   verbose: console.log,
 });
+
+if (!fs.existsSync("public/images")) {
+  fs.mkdirSync("public/images");
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "public/images"),
+  filename: (req, file, cb) =>
+    cb(null, Date.now() + path.extname(file.originalname)),
+});
+const upload = multer({ storage });
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -29,6 +43,68 @@ router.get("/products/:id", function (req, res, next) {
     title: product.name,
     product: product,
   });
+});
+
+router.get("/basket", function (req, res, next) {
+  const selectProducts = db.prepare("SELECT * FROM products");
+  const products = selectProducts.all();
+
+  res.render("basket", {
+    title: "Basket",
+    products: products,
+  });
+});
+
+router.get("/checkout", function (req, res, next) {
+  const selectProducts = db.prepare("SELECT * FROM products");
+  const products = selectProducts.all();
+
+  res.render("checkout", {
+    title: "Checkout",
+    products: products,
+  });
+});
+
+router.get("/admin/products", function (req, res, next) {
+  const selectProducts = db.prepare("SELECT * FROM products");
+  const products = selectProducts.all();
+
+  res.render("admin", {
+    title: "Admin - Products",
+    products: products,
+  });
+});
+
+router.get("/admin/new", function (req, res, next) {
+  res.render("new", {
+    title: "Admin - New Product",
+  });
+});
+
+router.post("/admin/new", upload.single("picture"), function (req, res, next) {
+  console.log("BODY:", req.body); // <== payloadkoll
+  console.log("FILE:", req.file); // <== Bildkoll
+
+  if (!req.file) {
+    return res.status(400).send("Ingen fil mottagen");
+  }
+
+  // lägga till produkt i databasen
+  const insertProduct = db.prepare(
+    "INSERT INTO products (name, description, price, picture, brand, SKU, publication_date) VALUES (?, ?, ?, ?, ?, ?, ?)"
+  );
+
+  const info = insertProduct.run(
+    req.body.name,
+    req.body.description,
+    req.body.price,
+    req.file.filename,
+    req.body.brand || "",
+    req.body.SKU,
+    req.body.publication_date
+  );
+
+  res.redirect("/admin/products");
 });
 
 module.exports = router;
